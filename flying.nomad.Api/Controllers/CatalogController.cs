3fb45d0aa076665ab78272d25b4af44d5cf575b5
James Pentecost <Pentecost.17@osu.edu>
2025-03-24 16:23:24 -0400

    CatalogController Correction

--- flying.nomad.Api/Controllers/CatalogController.cs
+++ flying.nomad.Api/Controllers/CatalogController.cs
@@ -1,14 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using flying.nomad.Domain.Catalog;
 
-namespace desert.deer.Api.Controllers {
+namespace flying.nomad.Api.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class CatalogController : ControllerBase {
-        
-        [HttpGet]
-        public IActionResult GetItems() {
-            return Ok("Hello World!");
-        }
+public class CatalogController : ControllerBase {
+    [HttpGet]
+    public IActionResult GetItems() {
+        var items = new List<Item>() {
+            new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m),
+            new Item("Shorts", "Ohio State Shorts", "Nike", 44.99m)
+        };
+        return Ok(items);
     }
 }
+}
+
