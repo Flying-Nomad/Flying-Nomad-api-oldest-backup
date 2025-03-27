e04c3427b5a4b027e3bb9a20646a4bfd62c8db3f
James Pentecost <Pentecost.17@osu.edu>
2025-03-26 21:16:18 -0400

    Created DbInitializer

--- flying.nomad.Data/StoreContext.cs
+++ flying.nomad.Data/StoreContext.cs
@@ -1,9 +1,17 @@
 ﻿using flying.nomad.Domain.Catalog;
 using Microsoft.EntityFrameworkCore;
+using flying.nomad.Data.Initializers; // Ensure this namespace contains DbInitializer
 
 namespace flying.nomad.Data {
     public class StoreContext : DbContext {
         public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
         public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+{
+    base.OnModelCreating(builder);
+            DbInitializer.Initialize(builder);
+}
+
         }
     }
